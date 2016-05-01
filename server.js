b92a2d6f1ab66bf380e976342cc53b4b62ca483d
CodeKittey <wing121@web.de>
2016-05-01 16:24:08 +0200

    :sparkles: fixes for CORS headers

--- server.js
+++ server.js
@@ -4,41 +4,34 @@ var playersAll = require('./data/playersAll.json');
 var playersFav = require('./data/playersFav.json');
 
 var restify = require('restify');
-restify.CORS.ALLOW_HEADERS.push('authorization');
-// Require the cors middleware for restify
-var corsMiddleware = require('restify-cors-middleware');
 
-// Require swagger to build an API documentation
-var swagger = require('swagger-restify');
-
-// Require node.js' path module
-var path = require('path');
-var server = restify.createServer();
+var server = restify.createServer({
+	name: 'WAWSS16'
+});
 server.use(restify.bodyParser());
 server.use(restify.CORS());
 server.use(restify.queryParser());
 
 
-server.get('/api/players', function(req, res, next){
+server.get('/api/players', (req, res) => {
 	var query = req.params.favorites || 'false';
 
 	if(query === 'true'){
-		res.send(200, playersFav);
+		res.json(200, playersFav);
 	} else if(query === 'false'){
-		res.send(200, playersAll);
+		res.json(200, playersAll);
 	} else {
-		res.send(404, JSON.stringify({message: 'FAIL'}));
+		res.json(404, { message: 'FAIL' });
 	}
-	res.send(query);
 });
-server.post('/api/players', function(req, res, next){
-	if(req.is('application/json')){
-			res.send(200, JSON.stringify({message: 'sucess'}));
-	} else {
-			res.send(404, JSON.stringify({message: 'FAIL'}));
+server.post('/api/players', (req, res) => {
+	if(req.body) {
+		return res.json(200, { message: 'success' });
 	}
+
+	res.json(404, { message: 'Empty body is not allowed.' });
 });
 
-server.listen(13337, function() {
-  console.log('%s listening at %s', server.name, server.url);
+server.listen(13337, () => {
+ console.log(`${server.name} is listening at ${server.url}`);
 });
