b2d972603808412ecfb0c07ce4d5e1b6d353a9e4
CodeKittey <wing121@web.de>
2016-11-30 00:43:34 +0100

    check REST API

--- server.js
+++ server.js
@@ -1,38 +1,131 @@
 'use strict';
+const _ = require('underscore');
 
-var playersAll = require('./data/playersAll.json');
-var playersFav = require('./data/playersFav.json');
+var avatars = require('./data/avatar.json');
+var students = require('./data/students.json');
+var competences = require('./data/competences.json');
+var illustrations = require('./data/illustrations.json');
+
+const webtoken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiSmVucyBEb29zZSJ9.uSVgj_vIPkX8PGQvAY_v2AfFAzntdT6uhOiuhveFrbw';
 
 var restify = require('restify');
+const baseUrl = '/api/V1/';
 
 var server = restify.createServer({
-	name: 'WAWSS16'
+    name: 'WAWWS16'
 });
 server.use(restify.bodyParser());
 server.use(restify.CORS());
 server.use(restify.queryParser());
 
-
-server.get('/api/players', (req, res) => {
-	var query = req.params.favorites || 'false';
-
-		console.log(req.params);
-		if (query === 'true') {
-		res.json(200, playersFav);
-	} else if (query === 'false') {
-		res.json(200, playersAll);
-	} else {
-		res.json(404, { message: 'FAIL' });
-	}
+server.put(baseUrl + 'login', (req, res) => {
+    res.json(200, {token: webtoken});
 });
-server.post('/api/players', (req, res) => {
-	if (req.body) {
-		return res.json(200, { message: 'success' });
-	}
 
-	res.json(404, { message: 'Empty body is not allowed.' });
+server.put(baseUrl + 'passwordRecovery', (req, res) => {
+    var authHeader = req.headers.authorization;
+    if (!authHeader) {
+        res.json(401, {message: 'Keine Berechtigung - fehlender Token'});
+    } else {
+        res.json(200, {message: "Passwort erfolgreich geaendert", token: webtoken});
+    }
+});
+
+server.del(baseUrl + 'student', (req, res) => {
+    var authHeader = req.headers.authorization;
+    if (!authHeader) {
+        res.json(401, {message: 'Keine Berechtigung - fehlender Token'});
+    } else {
+        res.json(200, {message: "Student wurde erfolgreich geloescht"});
+    }
+});
+
+server.get(baseUrl + 'student', (req, res) => {
+    var authHeader = req.headers.authorization;
+    if (!authHeader) {
+        res.json(401, {message: 'Keine Berechtigung - fehlender Token'});
+    } else {
+        res.json(200, students[0]);
+    }
+});
+
+server.get(baseUrl + 'avatar/:avatarId', (req, res) => {
+    var aId = req.params.avatarId || 1;
+
+    var authHeader = req.headers.authorization;
+    if (!authHeader) {
+        res.json(401, {message: 'Keine Berechtigung - fehlender Token'});
+    } else {
+        var avatar = _.find(avatars, (avatarObj) => {
+            if (avatar._id === aId)
+                return avatarObj;
+        });
+        res.json(200, avatar);
+    }
+});
+
+server.get(baseUrl + 'avatar', (req, res) => {
+    var authHeader = req.headers.authorization;
+    if (!authHeader) {
+        res.json(401, {message: 'Keine Berechtigung - fehlender Token'});
+    } else {
+        res.json(200, avatars);
+    }
+});
+
+server.get(baseUrl + 'chapterillustrations/:chapterid', (req, res) => {
+    var authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+        res.json(401, {message: 'Keine Berechtigung - fehlender Token'});
+    } else {
+        res.json(200, illustrations);
+    }
+});
+
+server.get(baseUrl + 'studentcompetence', (req, res) => {
+    var authHeader = req.headers.authorization;
+    var checked = req.params.checked ? true : false;
+    var chapterId = parseInt(req.params.chapterId) || 1;
+    var filteredCompetences = [];
+
+    if (!authHeader) {
+        res.json(401, {message: 'Keine Berechtigung - fehlender Token'});
+    } else {
+        filteredCompetences = competences.filter((competence) => {
+            return competence.chapterId === chapterId;
+        });
+
+        if (checked) {
+            var filteredCompetencesChecked = filteredCompetences.filter((competence) => {
+                return competence.checked === true;
+            });
+
+            res.json(200, filteredCompetencesChecked);
+        } else {
+            res.json(200, filteredCompetences);
+        }
+
+
+    }
+});
+
+
+server.get(baseUrl + 'educationalPlan/:educationplanId', (req, res) => {
+    var authHeader = req.headers.authorization;
+    var educationplanId = parseInt(req.params.educationplanId) || 1;
+    var filteredCompetences = [];
+
+    if (!authHeader) {
+        res.json(401, {message: 'Keine Berechtigung - fehlender Token'});
+    } else {
+        filteredCompetences = competences.filter((competence) => {
+            return competence.educationplanId === educationplanId;
+        });
+        res.json(200, filteredCompetences);
+    }
 });
 
 server.listen(1337, () => {
- console.log(`${server.name} is listening at ${server.url}`);
+    console.log(`${server.name} is listening at ${server.url}`);
 });
